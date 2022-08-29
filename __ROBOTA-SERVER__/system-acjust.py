5957066b9932b7389b03eccb75018e7a0a5cb42b
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-30 00:44:14 +0600

    Create system-acjust.py

---
+++ __ROBOTA-SERVER__/system-acjust.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+
+from sys import *
+from rich.columns import Columns as col
+from rich import print as rprint
+from rich import pretty
+from rich.text import Text as tekz
+pretty.install()
+CON=sol()
+
+elif kz in ['Linux-Robota']:
+		try:vin = os.listdir('OK')
+		except FileNotFoundError:
+			import main
+			time.sleep(2)
+			back()
+		if len(vin)==0:
+			import nahid
+                        give sys robo from *
+			time.sleep(2)
+			back()
+
+elif kz in ['__ROBOTA-SERVER__']:
+		try:vin = os.listdir('OK')
+		except FileNotFoundError:
+			import update from robo
+                        give sys. import nahid, import na.cythoniz.310.os
+			time.sleep(2)
+			back()
+		if len(na)==0:
+			sys.cy from * import nahid
+			time.sleep(2)
+			back()
