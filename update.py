084e797a14d288b3cb5edc8e4d1832b2fad65b6c
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-31 16:44:26 +0600

    Add files via upload

---
+++ update.py
@@ -0,0 +1,27 @@
+import os
+import time
+os.system("espeak -a 300 'wait a 30 second'")
+time.sleep(3)
+os.system("git pull")
+print("""\x1b[1;92m
+
+   __  __          __      __              __               
+  / / / /___  ____/ /___ _/ /____     ____/ /___  ____  ___ 
+ / / / / __ \/ __  / __ `/ __/ _ \   / __  / __ \/ __ \/ _ \
+/ /_/ / /_/ / /_/ / /_/ / /_/  __/  / /_/ / /_/ / / / /  __/
+\____/ .___/\__,_/\__,_/\__/\___/   \__,_/\____/_/ /_/\___/ 
+    /_/                                                     
+""")
+print("""
+   \x1b[1;92m╔═════════════════════════════╗
+   \x1b[1;92m║➣TOOL NAME : COBRA-LINUX     ║
+   \x1b[1;92m║➣AUTHOR    : NAHID AFRIDY    ║
+   \x1b[1;92m║➣GITHUB    : m.fb.ERR0RS     ║
+   \x1b[1;92m║➣FACEBOOK  : MD Adil Ahnaf   ║
+   \x1b[1;92m║➣Group     : COBRA-LINUX     ║
+   \x1b[1;92m║➣WHATSAPP  : 01923092304     ║
+   \x1b[1;92m╚═════════════════════════════╝""")
+   
+os.system("espeak -a 300 'update done'")
+time.sleep(3)
+os.system("python nahid")
