9e55c66df7a71bb14160aeefb6da19b4202d2aea
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-31 21:47:01 +0600

    Update Fk.py

--- Fk.py
+++ Fk.py
@@ -1,5 +1,4 @@
 import os
 os.system("espeak -tt 300 Wait a few minutes Sir")
 os.system("mpv __ROBOTA-SERVER__/bv/mz.mp3")
-os.system("mpv __ROBOTA-SERVER__ && bv && mz.mp3")
 os.system("python robo")
